ab190226cbb014fb7bdbf9fccd013d2f3abce698
Ashahab007 <ashahab007@gmail.com>
2025-07-28 01:17:22 +0600

    created custom logo in navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
+import logo from "../../assets/images/ammar_shahab_logo.png";
 import {
-  Button,
   Navbar,
   NavbarBrand,
   NavbarCollapse,
@@ -16,18 +16,13 @@ const NavBar = () => {
         fluid
         rounded
       >
-        <NavbarBrand>
-          <img
-            src="/favicon.svg"
-            className="mr-3 h-6 sm:h-9"
-            alt="Flowbite React Logo"
-          />
+        <NavbarBrand className="">
+          <img src={logo} className="h-14 md:h-20" alt="Logo" />
         </NavbarBrand>
         <NavbarToggle />
         <NavbarCollapse>
           <NavbarLink
             className="relative inline-block group py-2 px-3 md:py-6 text-white hover:!text-white border-b-0 hover:!bg-[#0a0d0d]"
-            /*  className="relative inline-block group py-2 px-3 md:py-6 text-white md:dark:hover:text-blue-500" */
             href="#"
           >
             Home
@@ -54,7 +49,7 @@ const NavBar = () => {
             Projects
             <span className="absolute left-0 bottom-0 h-1 w-full scale-x-0 origin-left bg-amber-300 transition-transform duration-300 group-hover:scale-x-100"></span>
           </NavbarLink>
-          <div className="flex flex-col justify-start sm:items-center sm:justify-center">
+          <div className="flex flex-col justify-start sm:items-center sm:justify-center mt-1 sm:mt-0">
             <button
               type="button"
               className="text-white bg-[#F7BE38] hover:bg-gray-800 transition-colors duration-300 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/20 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 me-2 mb-2 "
