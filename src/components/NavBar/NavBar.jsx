d40e5c722e3ff2e59e4943082b39f4cd3490cf14
Ammar Shahab <ammar.qa@generalpharma.com>
2025-08-03 15:54:08 +0600

    updated the navlink background color

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -22,28 +22,28 @@ const NavBar = () => {
         <NavbarToggle />
         <NavbarCollapse>
           <NavbarLink
-            className="relative inline-block group py-2 px-3 md:py-6 text-white hover:!text-white border-b-0 hover:!bg-[#0a0d0d]"
+            className="relative inline-block group py-2 px-3 md:py-6 text-white hover:!text-white border-b-0 hover:!bg-[#000000]"
             href="#"
           >
             Home
             <span className="absolute left-0 bottom-0 h-1 w-full scale-x-0 origin-left bg-amber-300 transition-transform duration-300 group-hover:scale-x-100"></span>
           </NavbarLink>
           <NavbarLink
-            className="relative inline-block group py-2 px-3 md:py-6 text-white hover:!text-white border-b-0 hover:!bg-[#0a0d0d]"
+            className="relative inline-block group py-2 px-3 md:py-6 text-white hover:!text-white border-b-0 hover:!bg-[#000000]"
             href="#experience"
           >
             Experience
             <span className="absolute left-0 bottom-0 h-1 w-full scale-x-0 origin-left bg-amber-300 transition-transform duration-300 group-hover:scale-x-100"></span>
           </NavbarLink>
           <NavbarLink
-            className="relative inline-block group py-2 px-3 md:py-6 text-white hover:!text-white border-b-0 hover:!bg-[#0a0d0d]"
+            className="relative inline-block group py-2 px-3 md:py-6 text-white hover:!text-white border-b-0 hover:!bg-[#000000]"
             href="#skills"
           >
             Skills
             <span className="absolute left-0 bottom-0 h-1 w-full scale-x-0 origin-left bg-amber-300 transition-transform duration-300 group-hover:scale-x-100"></span>
           </NavbarLink>
           <NavbarLink
-            className="relative inline-block group py-2 px-3 md:py-6 text-white hover:!text-white border-b-0 hover:!bg-[#0a0d0d]"
+            className="relative inline-block group py-2 px-3 md:py-6 text-white hover:!text-white border-b-0 hover:!bg-[#000000]"
             href="#projects"
           >
             Projects
