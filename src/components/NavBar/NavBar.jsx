5b9c69b3581500d533497fe9a711e2dd78eb48ef
Ammar Shahab <ammar.qa@generalpharma.com>
2025-08-11 15:08:48 +0600

    changed the layout of download cv buttonr

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -18,9 +18,9 @@ const NavBar = () => {
     document.body.removeChild(link);
   };
   return (
-    <nav className="bg-[#000000] border-gray-200 dark:bg-gray-900 font-poppins">
+    <nav className="bg-[#000000] border-gray-200 dark:bg-gray-900 font-poppins ">
       <Navbar
-        className="max-w-6xl bg-[#000000] mx-auto px-2 dark:bg-gray-900"
+        className="max-w-6xl bg-[#000000] mx-auto px-2 dark:bg-gray-900 "
         fluid
         rounded
       >
