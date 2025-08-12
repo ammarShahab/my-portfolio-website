05dcfdf02d6c4d0d5b82f584c3490ade82b82f3e
Ashahab007 <ashahab007@gmail.com>
2025-08-12 23:01:38 +0600

    change the download resume button classes for responsiveness

--- src/components/shared/DownloadResumeBtn.jsx
+++ src/components/shared/DownloadResumeBtn.jsx
@@ -12,7 +12,7 @@ const DownloadResumeBtn = () => {
   return (
     <button
       onClick={handleDownloadCv}
-      className="text-white h-10 mt-3 bg-[#F7BE38] hover:bg-gray-800 transition-colors duration-300 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/20 font-medium rounded-lg text-sm px-5 text-center dark:focus:ring-[#F7BE38]/50 "
+      className="text-white h-10 mt-3 bg-[#F7BE38] hover:bg-gray-800 transition-colors duration-300 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/20 font-medium rounded-lg text-sm px-3 lg:px-5 text-center dark:focus:ring-[#F7BE38]/50 "
     >
       Download Resume
     </button>
